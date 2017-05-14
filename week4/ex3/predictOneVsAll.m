cf2e53bda3450efacb70152edea715b905dec5f2
Isaac <isaac.chio@laxino.com>
2017-05-15 01:02:58 +0800

    completed netural network

--- week4/ex3/predictOneVsAll.m
+++ week4/ex3/predictOneVsAll.m
@@ -29,8 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
+  z = X* all_theta';
+  [max_value,p] = max(sigmoid(z),[],2);
 
 
 
